4568ce6fcf92c488b8763b85a1f8db08bdb9f3ba
George Shore <george@georgeshore.com>
2016-02-29 17:18:04 +0000

    Change scene on button click

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
     public void LoadLevel(string name)
     {
         Debug.Log("LoadLevel requested for: " + name);
+        SceneManager.LoadScene(name);
     }
 
     public void QuitGame()
